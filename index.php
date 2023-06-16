f27ba2cb372b48e4cef060ca17933a2d16492854
farazhashmi21 <farazhashmi9000@gmail.com>
2023-06-16 17:23:07 +0000

    Front-Page Conplete

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body onLoad = "displ();">
+	<!-- Header Start -->
 <div class="grid-header">
 	<div class="grid-items-header">
 		<p id="logoElement">cS</p>
@@ -16,6 +17,78 @@
 		<p>Clogan</p>
 	</div>
 </div>
+<!-- Header End -->
+<!-- Navigation Start -->
+<div class="main-nav">
+	<ul>
+		<li><a href="#" title="Homepage">Home</a></li>
+		<li><a href="#" title="Aboutpage">About</a></li>
+		<li><a href="#" title="Servicespage">Services</a></li>
+		<li><a href="#" title="Productspage">Products</a></li>
+		<li><a href="#" title="Blogpage">Blog</a></li>
+		<li><a href="#" title="Contactpage">Contact</a></li>
+	</ul>
+</div>
+<!-- Navigation End -->
+<!-- Hero Section Start -->
+<div class="hero-section">
+	<h2>Core Site</h2>
+</div>
+<!-- Hero Section End -->
+<!-- Main Body Section Start -->
+<div class="main-body">
+	<div class="body-items">
+		<h3>Heading</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+	<div class="body-items">
+		<h3>Heading</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+	<div class="body-items">
+		<h3>Heading</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+</div>
+<!-- Main Body Section End -->
+<!-- Testimonials Section Start -->
+<div class="testimonials-section">
+	<div class="testimonials-heading">
+		<h3>Testimonials</h3>
+	</div>
+	<div class="testimonials-grid">
+		<div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+		<div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+		<div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+	</div>
+</div>
+<!-- Testimonials Section End -->
+<!-- Footer Section Starts -->
+<footer class="main-footer">
+<p>&#169; - Copyright - <script>document.write(new Date().getFullYear());</script></p>	
+</footer>
+<!-- Footer Section Ends -->
 <script type="text/javascript" src="assets/js/functions.js"></script>
 </body>
 </html>
