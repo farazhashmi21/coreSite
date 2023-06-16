42758c31f923345757a869d476935ec68d1bfb6b
farazhashmi21 <farazhashmi9000@gmail.com>
2023-06-16 17:28:03 +0000

    Quick Fix

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="en-US">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<title>Core Site</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
 </head>
 <body onLoad = "displ();">
 	<!-- Header Start -->
