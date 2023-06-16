fcf43aad35ef2e4086dd728fbd03ce15ae795d6d
farazhashmi21 <farazhashmi9000@gmail.com>
2023-06-16 14:32:46 +0000

    FIrst Commit and Header Set

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # coreSite
 PHP Core Website Without Any CMS And Framework.
+
+Task Assigned:
+Make a website template using CSS Grid style.
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Core Site</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body onLoad = "displ();">
+<div class="grid-header">
+	<div class="grid-items-header">
+		<p id="logoElement">cS</p>
+	</div>
+	<div class="grid-items-header" id="logo-grid-span">
+		<h1>Core Site</h1>
+		<p>Clogan</p>
+	</div>
+</div>
+<script type="text/javascript" src="assets/js/functions.js"></script>
+</body>
+</html>
