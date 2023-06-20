24ca3095fe7d16f8b2f8782c696a35a7adb19790
farazhashmi21 <farazhashmi9000@gmail.com>
2023-06-20 15:50:56 +0000

    Universal Links File

---
+++ main-nav-links.php
@@ -0,0 +1,13 @@
+
+<!-- Navigation Start -->
+<div class="main-nav">
+	<ul>
+		<li><a href="index.php" title="Homepage">Home</a></li>
+		<li><a href="about-us.php" title="Aboutpage">About</a></li>
+		<li><a href="#" title="Servicespage">Services</a></li>
+		<li><a href="#" title="Productspage">Products</a></li>
+		<li><a href="#" title="Blogpage">Blog</a></li>
+		<li><a href="#" title="Contactpage">Contact</a></li>
+	</ul>
+</div>
+<!-- Navigation End -->
