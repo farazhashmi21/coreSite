dd73cc3b3d433e0210f31090972f4b7bc9c27361
farazhashmi21 <farazhashmi9000@gmail.com>
2023-06-20 16:40:19 +0000

    About Us Page Created

---
+++ about-us.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<title>About Us | Core Site</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
+</head>
+<body onLoad = "displ();">
+	<!-- Header Start -->
+<div class="grid-header">
+	<div class="grid-items-header">
+		<p id="logoElement">cS</p>
+	</div>
+	<div class="grid-items-header" id="logo-grid-span">
+		<h1>Core Site</h1>
+		<p>Clogan</p>
+	</div>
+</div>
+<!-- Header End -->
+<!-- Navigation File Add Start -->
+<?php
+  require('main-nav-links.php');
+?>
+<!-- Navigation File Add End -->
+<!-- Hero Section Start -->
+<div class="hero-section">
+	<h2>Core Site</h2>
+</div>
+<!-- Hero Section End -->
+<!-- Main Body Section Start -->
+<div class="main-body">
+	<div class="body-items body-text-padding">
+		<h3>Heading</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+	<div class="body-items body-items-image">
+		<img src="assets/images/header-hero.jpg" alt="header-hero.jpg">
+	</div>
+</div>
+<div class="main-body grid-item-two">
+	<div class="body-items body-items-image">
+		<img src="assets/images/header-hero.jpg" alt="header-hero.jpg">
+	</div>
+	<div class="body-items body-text-padding">
+		<h3>Heading</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+</div>
+<div class="main-body">
+	<div class="body-items body-text-padding">
+		<h3>Heading</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+	<div class="body-items body-items-image">
+		<img src="assets/images/header-hero.jpg" alt="header-hero.jpg">
+	</div>
+</div>
+<div class="main-body grid-item-two">
+	<div class="body-items body-items-image">
+		<img src="assets/images/header-hero.jpg" alt="header-hero.jpg">
+	</div>
+	<div class="body-items body-text-padding">
+		<h3>Heading</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+</div>
+<!-- Main Body Section End -->
+<!-- Footer Section Starts -->
+<footer class="main-footer">
+<p>&#169; - Copyright - <script>document.write(new Date().getFullYear());</script></p>	
+<p><a href="https://codepen.io/collection/MgEeLW/" title="https://codepen.io/collection/MgEeLW">Collection</a> Of The Developer <q>Engr. Faraz Hashmi</q></p>
+</footer>
+<!-- Footer Section Ends -->
+<script type="text/javascript" src="assets/js/functions.js"></script>
+</body>
+</html>
