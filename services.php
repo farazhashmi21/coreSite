735094c58a2475e7399f8800c6dfe1112d0edf85
farazhashmi21 <farazhashmi9000@gmail.com>
2023-06-21 15:29:11 +0000

    Services Page Created

---
+++ services.php
@@ -0,0 +1,9 @@
+<?php
+  require('header.php');
+?>
+<div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae temporibus possimus ipsam fuga aut, aliquam cupiditate ducimus repellat porro eum quis commodi. Praesentium necessitatibus harum maxime nobis ea debitis commodi.</p>
+</div>
+<?php
+  require('footer.php');
+?>
