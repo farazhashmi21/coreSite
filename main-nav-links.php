d13da934da04920685dd7d38d69c5f10831c2dfa
farazhashmi21 <farazhashmi9000@gmail.com>
2023-06-21 15:31:11 +0000

    Services Page Has Been Added To The Main Navigation File

--- main-nav-links.php
+++ main-nav-links.php
@@ -4,7 +4,7 @@
 	<ul>
 		<li><a href="index.php" title="Homepage">Home</a></li>
 		<li><a href="about-us.php" title="Aboutpage">About</a></li>
-		<li><a href="#" title="Servicespage">Services</a></li>
+		<li><a href="services.php" title="Servicespage">Services</a></li>
 		<li><a href="#" title="Productspage">Products</a></li>
 		<li><a href="#" title="Blogpage">Blog</a></li>
 		<li><a href="#" title="Contactpage">Contact</a></li>
