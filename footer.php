0dfcdf678f74c38bffb0b1764730a5b3bb66cef1
farazhashmi21 <farazhashmi9000@gmail.com>
2023-06-21 15:40:42 +0000

    Main Footer Of The CoreSite Has Been Updated

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <!-- Footer Section Starts -->
 <footer class="main-footer">
-<p>&#169; - Copyright - <script>document.write(new Date().getFullYear());</script></p>	
-<p><a href="https://codepen.io/collection/MgEeLW/" title="https://codepen.io/collection/MgEeLW">Collection</a> Of The Developer <q>Engr. Faraz Hashmi</q></p>
+<p>CoreSite - &#169; - Copyright - <script>document.write(new Date().getFullYear());</script></p>	
+<p><a href="https://codepen.io/tv/MgEeLW/" title="https://codepen.io/tv/MgEeLW">Collection</a> Of The Developer <q>Engr. Faraz Hashmi</q></p>
 </footer>
 <!-- Footer Section Ends -->
 <script type="text/javascript" src="assets/js/functions.js"></script>
