ab35df7de9976c8fbd899cfa9391e7244a54f2c4
farazhashmi21 <farazhashmi9000@gmail.com>
2023-06-27 15:55:42 +0000

    Services Page Desined!

--- services.php
+++ services.php
@@ -1,8 +1,23 @@
 <?php
   require('header.php');
 ?>
-<div>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae temporibus possimus ipsam fuga aut, aliquam cupiditate ducimus repellat porro eum quis commodi. Praesentium necessitatibus harum maxime nobis ea debitis commodi.</p>
+<div class="services-main-body">
+  <div class="body-items">
+    <div class="services-body">
+      <img src="assets/images/header-hero.jpg" alt="header-hero" width="100%">
+      <h2 class="center-heading-service">Web Development</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae temporibus possimus ipsam fuga aut, aliquam cupiditate ducimus repellat porro eum quis commodi. Praesentium necessitatibus harum maxime nobis ea debitis commodi.</p>
+      <a href="contact.php" class="contact-button-service">Contact</a>
+    </div>
+  </div>
+  <div class="body-items">
+    <div class="services-body">
+      <img src="assets/images/header-hero.jpg" alt="header-hero" width="100%">
+      <h2 class="center-heading-service">Search Engine Optimization</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae temporibus possimus ipsam fuga aut, aliquam cupiditate ducimus repellat porro eum quis commodi. Praesentium necessitatibus harum maxime nobis ea debitis commodi.</p>
+      <a href="contact.php" class="contact-button-service">Contact</a>
+    </div>
+  </div>
 </div>
 <?php
   require('footer.php');
