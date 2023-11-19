809848c447523aeb01ee14fee7e7e34bac503859
farazhashmi21 <farazhashmi9000@hotmail.com>
2023-11-19 17:50:12 +0000

    Zinger Burger Blog Page Link Added

--- main-nav-links.php
+++ main-nav-links.php
@@ -7,6 +7,7 @@
 		<li><a href="services.php" title="Servicespage">Services</a></li>
 		<li><a href="#" title="Productspage">Products</a></li>
 		<li><a href="#" title="Blogpage">Blog</a></li>
+		<li><a href="zinger_burger_eat_out.php" title="ZInger Blogpage">Zinger</a></li>
 		<li><a href="#" title="Contactpage">Contact</a></li>
 	</ul>
 </div>
